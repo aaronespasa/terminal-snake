4f47d73be4208cd22218aa95d771e1d6ed1a4f52
aaronespasa <aaespasa@gmail.com>
2022-02-07 12:14:53 +0100

    Implementing the display and clear methods

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -18,8 +18,7 @@ void NCursesDisplay::updateHighScore(WINDOW* window, WINDOW* gameWindow, int win
     mvwprintw(window, 2, 2 * (int)(windowWidth / 3), highScoreStr.c_str());
     wrefresh(window);
 
-    // player stays in the same where it died
-    wmove(gameWindow, player.Y(), player.X());
+    wmove(gameWindow, player.body.front().Y(), player.body.front().X());
     wrefresh(gameWindow);     
 }
 
@@ -30,13 +29,15 @@ void NCursesDisplay::game(WINDOW* scoreWindow, WINDOW* gameWindow, int windowWid
     
     while(player.alive) {
         updatePoints(scoreWindow, gameWindow, windowWidth, player);
-        wmove(gameWindow, player.Y(), player.X());
+        displayPlayerElementInPosition(gameWindow, player.body.front());
         wrefresh(gameWindow);
+        
+        clearPlayerTailFromDisplay(gameWindow, player.body.front(), player.body.back());
 
         int c = getch();
 
         player.move(c, windowWidth - 1, windowHeight);
-        wmove(gameWindow, player.Y(), player.X());
+        wmove(gameWindow, player.body.front().Y(), player.body.front().X());
 
         std::this_thread::sleep_for(std::chrono::milliseconds(300 / difficulty));
     }
@@ -49,21 +50,37 @@ void NCursesDisplay::game(WINDOW* scoreWindow, WINDOW* gameWindow, int windowWid
     wrefresh(gameWindow);
 }
 
+void NCursesDisplay::clearPlayerTailFromDisplay(WINDOW* gameWindow,
+                                                PlayerElement &head,
+                                                PlayerElement &tail) {
+    wmove(gameWindow, tail.Y(), tail.X());
+    wprintw(gameWindow, " ");
+    wmove(gameWindow, head.Y(), head.X());
+}
+
+void NCursesDisplay::displayPlayerElementInPosition(WINDOW* gameWindow,
+                                                    PlayerElement &head) {
+    mvwprintw(gameWindow, head.Y(), head.X(), head.displayIcon.c_str());
+}
+
 /**
  * @brief Display the snake game on the terminal.
  * 
  */
 void NCursesDisplay::display(Player player) {
+    setlocale(LC_ALL, "");
     initscr();              // start ncurses (sets up memory & clears the screen)
     noecho();               // do not print input values
     cbreak();               // terminate on CTRL + C
     start_color();          // enable color
     keypad(stdscr, TRUE);   // gets the arrow input as a single int instead of 3
+    curs_set(0);
 
     // stdscr is the default window
     int width = getmaxx(stdscr);    
     int height = getmaxy(stdscr);
     int scoreWindowHeight = 5;
+    const int gameWindowHeight = height - scoreWindowHeight - 2;
     int startX = 0, startY = 0;
 
     WINDOW* scoreWindow = newwin(scoreWindowHeight, width, startY, startX);
@@ -81,13 +98,12 @@ void NCursesDisplay::display(Player player) {
     updatePoints(scoreWindow, gameWindow, width, player);
 
     bool userWantToContinuePlaying = true;
-    const int gameWindowHeight = height - scoreWindowHeight - 2;
 
     while(userWantToContinuePlaying) {
         // move the player to the initial point and restart default values
-        player.X(1, width);
-        player.Y(1, gameWindowHeight);
-        wmove(gameWindow, 1, 1);
+        player.respawn(width, gameWindowHeight);
+
+        displayPlayerElementInPosition(gameWindow, player.body.front());
         player.direction = RIGHT;
         player.points = 0;
         player.alive = true;
