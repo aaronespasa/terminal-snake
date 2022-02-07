386636e4036ecf202ccf7e9e26f1a6fadd8d5842
aaronespasa <aaespasa@gmail.com>
2022-02-07 11:41:08 +0100

    Converting the player into a composition of elements

--- include/player.h
+++ include/player.h
@@ -2,6 +2,8 @@
 #define PLAYER_H
 
 #include <unordered_map>
+#include <string>
+#include <deque>
 
 enum Direction {LEFT, UP, RIGHT, DOWN};
 
@@ -12,24 +14,32 @@ const std::unordered_map<int, Direction> ARROW_KEYS {
     {261, RIGHT}
 };
 
-class Player {
+class PlayerElement {
     public:
-        Player(int highscore = 0) {}
-
+        PlayerElement(int x, int y, int windowWidth, int windowHeight);
         int X();
         int Y();
         void X(int newX, int windowWidth);
         void Y(int newY, int windowHeight);
 
+        std::string displayIcon{"#"};
+        bool alive{true};
+    private:
+        int x{1};
+        int y{1};
+};
+
+class Player {
+    public:
+        std::deque<PlayerElement> body;
+
+        void respawn(int windowWidth, int windowHeight);
         void move(int keyNumber, int windowWidth, int windowHeight);     
 
         int points{0};
         int highScore{0};
         bool alive{true};
-        Direction direction = RIGHT;
-    private:
-        int x = 1;
-        int y = 1;
+        Direction direction{RIGHT};
 };
 
 #endif // !PLAYER_H
