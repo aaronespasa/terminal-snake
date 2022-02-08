adcc4ec2a8503980a427ce31696150ee25b59d21
aaronespasa <aaespasa@gmail.com>
2022-02-08 20:44:09 +0100

    Creating the header file for the food

---
+++ include/food.h
@@ -0,0 +1,30 @@
+#ifndef FOOD_H
+#define FOOD_H
+
+#include <vector>
+
+#include "player.h"
+
+struct Point {
+    int x;
+    int y;
+};
+
+class Food {
+    public:
+        Food(int windowHeight, int windowWidth);
+
+        int windowHeight();
+        int windowWidth();
+        void windowHeight(int newWindowHeight);
+        void windowWidth(int newWindowWidth);
+
+        void spawnFood(int number, std::deque<PlayerElement> playerBody);
+
+        std::vector<Point> foodsMap;
+    private:
+        int windowHeight;
+        int windowWidth;
+};
+
+#endif // !FOOD_H
